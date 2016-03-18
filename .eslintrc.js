2285b7b70c3510bcf9710a19efe7d252d76e7d20
Chris Bargren <cbargren@gmail.com>
2016-03-18 13:00:57 -0700

    Add a linter

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "comma-dangle": [2, "never"],
+        "no-param-reassign": [2, {"props": false}]
+    }
+};
